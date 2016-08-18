58542142272fc435c9ed820cd804fefa59958d16
Loc Vu <locvd89@gmail.com>
2016-08-18 00:43:11 -0400

    add database

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Parties } from '../both/collection/paties.collection';
 import template from './app.component.html';
 @Component({
     selector: 'app',
     template
 })
-export class AppComponent { }
+export class AppComponent {
+    parties: Mongo.Cursor<any>;
+    constructor() {
+        this.parties = Parties.find();
+    }
+}
